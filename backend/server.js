3251015fe5081af70c99a29aedb4c3b001f158e8
Ahmad <ahmad.bahr@mubs.edu.lb>
2025-03-05 19:45:38 +0200

    ....

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ import cors from "cors";
 import feedbackRoutes from "./routes/feedbackRoutes.js"; // Import feedback routes
 import adminRoutes from "./routes/adminRoutes.js"; // Import admin routes
 
+import path from "path";
+import { fileURLToPath } from "url";
+
 dotenv.config();
 
 const app = express();
@@ -27,3 +30,16 @@ mongoose.connect(process.env.MONGO_URI, {
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// For ES Modules (use this instead of __dirname)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+    });
+}
